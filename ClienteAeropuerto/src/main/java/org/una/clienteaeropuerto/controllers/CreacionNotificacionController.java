4d2b301e810b5fb18991c799e3bf15f917a1cccb
RacheVC <rachecordero12@gmail.com>
2020-10-21 17:04:36 -0600

    vista crear notificacion

--- ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/controllers/CreacionNotificacionController.java
+++ ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/controllers/CreacionNotificacionController.java
@@ -7,7 +7,14 @@ package org.una.clienteaeropuerto.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +23,19 @@ import javafx.fxml.Initializable;
  */
 public class CreacionNotificacionController implements Initializable {
 
+    @FXML
+    private TextField txtReceptor;
+    @FXML
+    private TextField txtAsunto;
+    @FXML
+    private TextArea txtMensaje;
+    @FXML
+    private RadioButton rbSi;
+    @FXML
+    private DatePicker dpCalendario;
+    @FXML
+    private Button btnGuardar;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +43,9 @@ public class CreacionNotificacionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void OnActionBtnGuardar(ActionEvent event) {
+    }
     
 }
