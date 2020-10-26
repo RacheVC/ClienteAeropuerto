4db82256a6ccfa68d20074db981dd6c60ad72152
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2020-10-26 15:29:13 -0600

    File Chooser aplicado correctamente

--- ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/controllers/CreacionNotificacionController.java
+++ ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/controllers/CreacionNotificacionController.java
@@ -5,6 +5,7 @@
  */
 package org.una.clienteaeropuerto.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,12 +18,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import org.una.clienteaeropuerto.App;
 import org.una.clienteaeropuerto.dto.NotificacionDTO;
 import org.una.clienteaeropuerto.service.NotificacionService;
@@ -37,61 +39,57 @@ public class CreacionNotificacionController implements Initializable {
     @FXML
     private TextField txtReceptor;
     @FXML
-    private TextField txtAsunto;
-    @FXML
     private TextArea txtMensaje;
-    @FXML
-    private RadioButton rbSi;
-    @FXML
-    private DatePicker dpCalendario;
     @FXML
     private Button btnGuardar;
     @FXML
     private Button btnAgregarImagen;
     LoginController user = new LoginController();
     
+    private Window stage;
+
     NotificacionDTO notificaciondto = new NotificacionDTO();
     NotificacionService notificacionservice = new NotificacionService();
-    
+    @FXML
+    private ImageView imgNotificacion;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void OnActionBtnGuardar(ActionEvent event) throws InterruptedException, ExecutionException, IOException {
-        int i=100;  
-       
-//        notificaciondto.setEmisor(user.CapturarUsuario());
+        int i = 100;
+
         notificaciondto.setEstado(true);
         notificaciondto.setMensaje(txtMensaje.getText());
         notificaciondto.setReceptor(txtReceptor.getText());
-//        notificaciondto.setAsunto();
-       
-        
+
         notificacionservice.add(notificaciondto);
-        
-        
-         Parent root = FXMLLoader.load(App.class.getResource("MantenimientoNotificaciones.fxml"));
+
+        Parent root = FXMLLoader.load(App.class.getResource("MantenimientoNotificaciones.fxml"));
         Scene creacionDocs = new Scene(root);
 
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(creacionDocs);
         window.show();
     }
 
     @FXML
     private void OnActionBtnAgregarImagen(ActionEvent event) {
-//        FileChooser fileChooser = new FileChooser();
-//        int seleccion;
-//        seleccion = fileChooser.showOpenDialog();
-//        
-//        if(seleccion == fileChooser.APROVE_OPTION){
-//            
-//        }
+
+        int result;
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"));
+        File imgFile = fc.showOpenDialog(stage);
+        if (imgFile != null) {
+            Image image = new Image("file:" + imgFile.getAbsolutePath());
+            imgNotificacion.setImage(image);
+        }
     }
-    
 }
