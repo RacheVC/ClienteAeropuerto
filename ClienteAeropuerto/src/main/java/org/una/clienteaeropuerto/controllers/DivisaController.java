df606c7464fc51e2d1abc38a962c3ae7b8049016
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-11-04 00:41:01 -0600

    Divisas Completo
    Se completo divisas ya validado

--- ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/controllers/DivisaController.java
+++ ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/controllers/DivisaController.java
@@ -11,11 +11,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import org.una.clienteaeropuerto.dto.DivisaDTO;
 import org.una.clienteaeropuerto.service.DivisaService;
 
@@ -24,11 +30,9 @@ import org.una.clienteaeropuerto.service.DivisaService;
  *
  * @author Andres
  */
-
 public class DivisaController implements Initializable {
 
-    
- private DivisaDTO divisalist ;
+    private DivisaDTO divisalist;
     @FXML
     private ComboBox<String> cmbTipoDivisa;
     @FXML
@@ -57,45 +61,147 @@ public class DivisaController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    cmbTipoDivisa.setValue("Eurodolar");
-    cmbTipoDivisa.getItems().addAll("Eurodolar","Yen","Dolar Americano");
-// TODO
-    }    
+        cmbTipoDivisa.setValue("Eurodolar");
+        cmbTipoDivisa.getItems().addAll("Dólar Americano", "Libra Esterlina", "Yen Japones", "Eurodolar", "Dólar Canadiense", "Franco Suizo", "Dólar neozelandes", "Dólar Australiano", "Colón Costarricense");
 
-    @FXML
-    private void Atras(ActionEvent event) {
-        
+        txtMonto.addEventHandler(KeyEvent.KEY_TYPED, event -> SoloNumerosEnteros(event));
+        CargarDatos();
+// TODO
+
     }
 
     @FXML
     private void CargarDivisas(ActionEvent event) throws InterruptedException, ExecutionException, IOException {
-        
-        if(cmbTipoDivisa.getValue() == "Dolar Americano"){
-           
-            Dolares();
+
+        CargarDivisasAutomaticamente();
+
+    }
+
+    public void CargarDivisasAutomaticamente() throws InterruptedException, ExecutionException, IOException {
+
+        if (cmbTipoDivisa.getValue() == "Dólar Americano") {
+
+            DolarAmericano();
+        }
+
+        if (cmbTipoDivisa.getValue() == "Libra Esterlina") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDGBP().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Yen Japones") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDJPY().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Eurodolar") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDEUR().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Dólar Canadiense") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDCAD().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Franco Suizo") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDCHF().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Dólar neozelandes") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDNZD().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Dólar Australiano") {
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDAUD().getRate()));
+        }
+
+        if (cmbTipoDivisa.getValue() == "Colón Costarricense") {
+
+            CalcularTipoCambio(Float.valueOf(divisalist.getRates().getUSDCRC().getRate()));
         }
     }
-    
-    public void Dolares() throws InterruptedException, ExecutionException, IOException{
-         double monto;
-            monto = Double.valueOf(txtMonto.getText());
+
+    public void CargarDatos() {
+        try {
             divisalist = (DivisaDTO) DivisaService.getInstance().getAll();
-            
-            txtDolarAmericano.setText(txtMonto.getText()); 
-            txtLibraEsterlina.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDGBP().getRate())*monto)));
-            txtYenJapones.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDJPY().getRate())*monto)));
-            
-            
-            txtEurodolar.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDEUR().getRate())*monto)));
-            txtDolarCanadiense.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDCAD().getRate())*monto)));
-            txtDolarNeozelandes.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDNZD().getRate())*monto)));
-            
-            txtFrancoSuizo.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDCHF().getRate())*monto)));
-            txtDolarAustraliano.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDAUD().getRate())*monto)));
-            txtColonCostarricense.setText(String.valueOf((Double.valueOf(divisalist.getRates().getUSDCRC().getRate())*monto)));
-            
-            System.out.println(divisalist.getRates().getUSDCAD().getRate());
-            System.out.println(divisalist.getRates().getUSDCAD().getRate());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(DivisaController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            Logger.getLogger(DivisaController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(DivisaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
+    public void DolarAmericano() throws InterruptedException, ExecutionException, IOException {
+        float monto;
+        monto = Float.valueOf(txtMonto.getText());
+        LlenartxtField(monto);
+        System.out.println(divisalist.getRates().getUSDCAD().getRate());
+        System.out.println(divisalist.getRates().getUSDCAD().getRate());
+    }
+
+    public void CalcularTipoCambio(float floatdivisa) {
+        float monto;
+        float montoEnDolar;
+
+        monto = Float.valueOf(txtMonto.getText());
+        System.err.println(monto);
+        montoEnDolar = monto / floatdivisa;
+        LlenartxtField(montoEnDolar);
+
+    }
+
+    public void LlenartxtField(float monto) {
+        txtDolarAmericano.setText(String.valueOf(monto));
+        txtLibraEsterlina.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDGBP().getRate()) * monto)));
+        txtYenJapones.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDJPY().getRate()) * monto)));
+
+        txtEurodolar.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDEUR().getRate()) * monto)));
+        txtDolarCanadiense.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDCAD().getRate()) * monto)));
+        txtDolarNeozelandes.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDNZD().getRate()) * monto)));
+
+        txtFrancoSuizo.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDCHF().getRate()) * monto)));
+        txtDolarAustraliano.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDAUD().getRate()) * monto)));
+        txtColonCostarricense.setText(String.valueOf((Float.valueOf(divisalist.getRates().getUSDCRC().getRate()) * monto)));
+    }
+
+    @FXML
+    private void Atras(ActionEvent event) {
+    }
+
+    @FXML
+    private void holdfdsf(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    private void BusquedaPorEscritura(KeyEvent event) throws InterruptedException, ExecutionException, IOException {
+
+        if (!txtMonto.getText().isEmpty()) {
+            CargarDivisasAutomaticamente();
+
+        } else {
+            txtColonCostarricense.clear();
+            txtDolarAmericano.clear();
+            txtDolarAustraliano.clear();
+            txtDolarCanadiense.clear();
+            txtDolarNeozelandes.clear();
+            txtEurodolar.clear();
+            txtFrancoSuizo.clear();
+            txtLibraEsterlina.clear();
+            txtMonto.clear();
+            txtYenJapones.clear();
+        }
+
+    }
+
+    public void SoloNumerosEnteros(KeyEvent keyEvent) {
+        try {
+            char key = keyEvent.getCharacter().charAt(0);
+            if (Character.isLetter(key)) {
+                keyEvent.consume();
+            }
+
+        } catch (Exception ex) {
+        }
+    }
+
 }
