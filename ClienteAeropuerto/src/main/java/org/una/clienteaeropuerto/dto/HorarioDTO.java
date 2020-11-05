5edb2a068b1841723fa043e7de06476581153434
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2020-11-04 19:30:11 -0600

    Modificación HorarioDTO

--- ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/dto/HorarioDTO.java
+++ ClienteAeropuerto/src/main/java/org/una/clienteaeropuerto/dto/HorarioDTO.java
@@ -5,6 +5,8 @@
  */
 package org.una.clienteaeropuerto.dto;
 
+import java.sql.Time;
+
 /**
  *
  * @author Andres
@@ -15,21 +17,26 @@ public class HorarioDTO {
     private String dia_Entrada;
     private String dia_Salida;
     private boolean estado;
+    private Time hora_entrada;
+    private Time hora_salida;
     private Areas_trabajoDTO areas_trabajo;
 
     public HorarioDTO() {
     }
 
-    @Override
-    public String toString() {
-        return "HorarioDTO{" + "id=" + id + ", dia_Entrada=" + dia_Entrada + ", dia_Salida=" + dia_Salida + ", areas_trabajo=" + areas_trabajo + '}';
-    }
-
-    public HorarioDTO(Long id, String dia_Entrada, String dia_Salida, Areas_trabajoDTO areas_trabajo) {
+    public HorarioDTO(Long id, String dia_Entrada, String dia_Salida, boolean estado, Time hora_entrada, Time hora_salida, Areas_trabajoDTO areas_trabajo) {
         this.id = id;
         this.dia_Entrada = dia_Entrada;
         this.dia_Salida = dia_Salida;
+        this.estado = estado;
+        this.hora_entrada = hora_entrada;
+        this.hora_salida = hora_salida;
         this.areas_trabajo = areas_trabajo;
+    }
+
+    @Override
+    public String toString() {
+        return "HorarioDTO{" + "id=" + id + ", dia_Entrada=" + dia_Entrada + ", dia_Salida=" + dia_Salida + ", estado=" + estado + ", hora_entrada=" + hora_entrada + ", hora_salida=" + hora_salida + ", areas_trabajo=" + areas_trabajo + '}';
     }
 
     public Long getId() {
@@ -71,4 +78,21 @@ public class HorarioDTO {
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
+
+    public Time getHora_entrada() {
+        return hora_entrada;
+    }
+
+    public void setHora_entrada(Time hora_entrada) {
+        this.hora_entrada = hora_entrada;
+    }
+
+    public Time getHora_salida() {
+        return hora_salida;
+    }
+
+    public void setHora_salida(Time hora_salida) {
+        this.hora_salida = hora_salida;
+    }
+
 }
